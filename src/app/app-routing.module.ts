7046bd282b7b0c855c3cfcd2c106e4f4ba335453
kajal <kajal.singh@oodlestechnologies.com>
2020-11-09 22:10:34 +0530

    Revert "Added form, table, and custom array table with directive"
    This reverts commit f953bf6b10278668a4363dfd326395d2ca7277f1.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserFormComponent } from './user-form/user-form.component';
-import { UserTableComponent } from './user-table/user-table.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { UserCustomArrayComponent } from './user-custom-array/user-custom-array.component';
 
-const routes: Routes = [
-  {path:'', component:MainPageComponent},
-  {path:'user-table', component:UserTableComponent},
-  {path:'user-form', component:UserFormComponent},
-  {path:'user-custom-table',component:UserCustomArrayComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserFormComponent } from './user-form/user-form.component';
-import { UserTableComponent } from './user-table/user-table.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { UserCustomArrayComponent } from './user-custom-array/user-custom-array.component';
-import { UppercaseDirective } from './user-custom-array/uppercase.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserFormComponent,
-    UserTableComponent,
-    MainPageComponent,
-    UserCustomArrayComponent,
-    UppercaseDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    NgxPaginationModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
